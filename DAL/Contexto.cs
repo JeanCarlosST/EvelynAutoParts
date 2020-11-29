56b4fc642c2c062f594d0f02be251cfd3ee1e619
David Maria <eldavid_46@hotmail.com>
2020-11-29 03:44:55 -0400

    Agregando Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entidades;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite(@"Data Source = Data/Prestamos.db");
+        }
+    }
+}
