108b8a7c85e372e84f079502239f584ddb7a656b
Kleyton72 <jeanc.1582@gmail.com>
2020-12-06 15:08:18 -0400

    Agregando entidades en OnModelCreating

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -29,6 +29,40 @@ namespace DAL
 
             modelBuilder.Entity<Usuarios>().HasData(new Usuarios(1, "jean", "admin", "Jean Carlos", "Santos"));
             modelBuilder.Entity<Usuarios>().HasData(new Usuarios(2, "david", "admin", "David", "Maria"));
+
+            modelBuilder.Entity<Productos>().HasData(new Productos()
+            {
+                ProductoId = 1,
+                UsuarioId = 1,
+                Descripcion = "Aceite",
+                Inventario = 100,
+                Precio = 60,
+                Costo = 30,
+                PorcentajeITBIS = 0.18f,
+                MargenGanancia = 0.4f,
+                MaxDescuento = 0.5f,
+            });
+
+            modelBuilder.Entity<Vendedores>().HasData(new Vendedores()
+            {
+                VendedorId = 1,
+                UsuarioId = 2,
+                Nombres = "Paco",
+                Apellidos = "Jimenez",
+                Comision = 0
+            });
+
+            modelBuilder.Entity<Clientes>().HasData(new Clientes()
+            {
+                ClienteId = 1,
+                UsuarioId = 1,
+                Nombres = "Juan",
+                Apellidos = "Perez",
+                Direccion = "Calle G",
+                Cedula = "05612345671",
+                Telefono = "8092348079",
+                Email = "juanp@gmail.com",
+            });
         } 
     }
 }
